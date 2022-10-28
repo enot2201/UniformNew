48ca3b2562d4a2ceb5b996e3090d70bfb7a44b6b
enot2201 <76753479+enot2201@users.noreply.github.com>
2022-10-29 02:44:01 +0300

    Delete ozon_service directory

--- ozon_service/ozon_service/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from ozon_service.Danger_data_api import views
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('ozon_service.Danger_data_api.urls'))
-
-]
